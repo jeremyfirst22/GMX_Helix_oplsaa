9cbdaba9072af07b85106f924afe54c76edf4377
jeremyfirst22 <jeremy_first@utexas.edu>
2017-07-19 16:04:50 -0500

    Added cutoff lines and legend

--- Process_scripts/Process_minimage.py
+++ Process_scripts/Process_minimage.py
@@ -6,6 +6,9 @@ from os import sys
 from matplotlib.colors import LogNorm
 import matplotlib.lines as mlines 
 
+xmax=100 ##ns
+cutoff = 10 ##Electrostatic cutoff distance (Angstroms) 
+
 figCols=1
 figRows=3
 
@@ -13,9 +16,9 @@ if not os.path.isdir('figures') :
     os.mkdir('figures') 
 
 fig, axarr = plt.subplots(figRows,figCols,sharex='col',sharey='row') 
-fig.subplots_adjust(wspace=0) 
-fig.text(0.5,0.04, "Time (ns)", ha='center', va='center') 
-fig.text(0.08,0.5, r"Distance to nearest image ($\AA$)", ha='center', va='center',rotation='vertical') 
+fig.subplots_adjust(left=0.1,right=0.8,top=0.9,bottom=0.1) 
+fig.text(0.5,0.05, "Time (ns)", ha='center', va='center') 
+fig.text(0.05,0.5, r"Distance to nearest image ($\AA$)", ha='center', va='center',rotation='vertical') 
 
 index=0
 for solvent in ['water','tert','sam'] : 
@@ -28,6 +31,9 @@ for solvent in ['water','tert','sam'] :
         except IOError : 
             print "No file found for %s %s"%(state,solvent)  
             continue
+        except ValueError :
+            print "Data import failed with ValueError. Trying without last line"
+            data = np.genfromtxt(datafile,skip_header=27,skip_footer=1) 
         if state == 'folded' : 
             c='b' 
         else :
@@ -36,15 +42,23 @@ for solvent in ['water','tert','sam'] :
         data[:,0] = data[:,0] / 1000 
         data[:,1] = data[:,1] * 10 # nm -> Angstroms
 
+        rvdw = np.full(len(data[:,1]),cutoff) 
+
         ax.scatter(data[:,0],data[:,1],s=0.1,color=c,label=state) 
+        ax.plot(data[:,0],rvdw,'r--')
+        ax.plot(data[:,0],2*rvdw,'k--') 
         ax.set_title('%s'%(solvent) ) 
-        ax.set_xlim(0,50) 
-        ax.set_ylim(10,50.0) 
+        ax.set_xlim(0,xmax) 
+        ax.set_ylim(0,60.0) 
 
     index +=1
 
-#blu_dot = mlines.Line2D([],[], linestyle='None',color='b', marker='o',label="folded") 
-#gre_dot = mlines.Line2D([],[], linestyle='None',color='g', marker='o',label="unfolded") 
+blu_dot = mlines.Line2D([],[], linestyle='None',color='b', marker='o',label="folded") 
+gre_dot = mlines.Line2D([],[], linestyle='None',color='g', marker='o',label="unfolded") 
+red_das = mlines.Line2D([],[],linestyle='--',color='r') 
+bla_das = mlines.Line2D([],[],linestyle='--',color='k') 
 
-#plt.legend(handles=[gre_dot,blu_dot],numpoints=1,loc='center left', bbox_to_anchor=(1.0, 1.75))
+fig.legend([blu_dot,gre_dot,red_das,bla_das],['folded','unfolded',r"R$_c$",r"2R$_c$"],numpoints=1,
+    loc='center', bbox_to_anchor=(0.9, 0.5),
+    fontsize='medium') 
 fig.savefig('figures/minimage.png',format='png') 
