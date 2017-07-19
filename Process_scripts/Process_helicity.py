effe6a06cff2e725a745a095dfc94fb7f9a4b66a
jeremyfirst22 <jeremy_first@utexas.edu>
2017-07-19 15:50:20 -0500

    Now plots all figures individually and on an figure array

--- Process_scripts/Process_helicity.py
+++ Process_scripts/Process_helicity.py
@@ -5,6 +5,10 @@ projectDir='/Users/jeremyfirst/GMX_Helix_oplsaa'
 rcFile='paper.rc'
 inFile='dssp/helen.nrt'
 saveDir='figures'
+outname='%s/combined_helical.pdf'%saveDir
+
+figCols=2
+figRows=3
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,16 +19,11 @@ import sys
 from matplotlib import rc_file
 
 def Usage():
-    print "Usage: %s <Data directory> <binSize>"%(sys.argv[0])
+    print "Usage: %s <binSize=10>"%(sys.argv[0])
 
-binSize = 1000
+binSize = 10
 try :
-    molec = sys.argv[1].strip("/") 
-except :
-    Usage()
-    sys.exit()
-try :
-    binSize = int(sys.argv[2])
+    binSize = int(sys.argv[1])
 except :
     Usage()
     print "No bin size indicated, defaulting to %i"%binSize
@@ -32,89 +31,117 @@ except :
 if not os.path.isdir(saveDir) : 
     os.mkdir(saveDir) 
 
-datafiles = glob.glob("%s/%s"%(molec,inFile) ) 
-curdir = os.path.abspath(os.getcwd())
-
-outname = "%s/binned_%s.pdf"%(saveDir,molec) 
-outname = os.path.join(curdir,outname)
-
-for filename in datafiles: 
-    print "Reading %s..."%filename 
-    datai = np.genfromtxt(filename,skip_header=29,skip_footer=1)
-    try : data = np.concatenate([data,datai])
-    except NameError : data = datai
-    print "\t Sizes: %i (%i)" %(len(datai), len(data))
-
-frameNumber = len(data)
-assert frameNumber > 0 
-
-binNumber = frameNumber/binSize
-
-print "Done reading data...binning" 
-
-y1 = data[:,1] # Alpha 
-y2 = data[:,2] # 3_10 Helix
-y3 = data[:,3] ##unfolded
-
-y1 /= 18 
-y2 /= 18 
-y3 /= 18 
-
-y1binned =[] 
-y2binned =[] 
-y3binned =[] 
-
-
-print frameNumber, " frames"
-print binSize, " frames per bin"
-print binNumber, "bins to be graphed"
-
-for j in range(binNumber):
-    sum1 = 0
-    sum2 = 0
-    sum3 = 0
-
-    for i in range(binSize):
-        sum1 += y1[i + j*binSize]
-        sum2 += y2[i + j*binSize] 
-        sum3 += y3[i + j*binSize] 
-
-    y1binned.append(sum1/binSize)
-    y2binned.append(sum2/binSize)
-    y3binned.append(sum3/binSize)
-
-y1binned = np.array(y1binned)
-y2binned = np.array(y2binned)
-y3binned = np.array(y3binned)
-
-print "Data binned.... now plotting"
-#rc_file('%s/rc_files/%s'%(projectDir,rcFile) ) 
-
-x = data[:,0] 
-x = np.linspace(0,np.max(data[:,0]),binNumber) 
-x /= 1000 
-#x = np.linspace(0,binSize*binNumber/ 10,binNumber) 
-
-plt.fill_between(x,0                ,y3binned         ,facecolor='r',linewidth=0.0)
-plt.fill_between(x,y3binned         ,y3binned+y2binned,facecolor='g',linewidth=0.0) 
-plt.fill_between(x,y3binned+y2binned,1                ,facecolor='b',linewidth=0.0) 
-
-print "Data plotted.... now labeling"
-
-plt.xlim(0,np.max(x)) 
-plt.ylim(0,1)
-
-plt.xlabel(r"time (ns)")
-plt.ylabel(r"\% helical structure")
+fig, axarr = plt.subplots(figRows, figCols, sharex='col',sharey='row') 
+fig.subplots_adjust(wspace=0.1) 
+fig.subplots_adjust(hspace=0.25) 
+fig.text(0.5,0.05, "Time (ns)", ha='center', va='center') 
+fig.text(0.05,0.5, r"Helical fraction",ha='center',va='center',rotation='vertical') 
+fig.subplots_adjust(left=0.1, bottom=0.1,right=0.80,top=0.9)
 
 blue = mpatches.Patch([],[],color='b',label=r"$\alpha$-helix")
 green= mpatches.Patch([],[],color='g',label=r"$3_{10}$-helix")
 red  = mpatches.Patch([],[],color='r',label=r"unfolded")
-plt.legend(handles=[blue, green, red],loc=4)
+
+index=0
+for row,solvent in enumerate(['water','sam','tert']): 
+    for col,state in enumerate(['folded','unfolded']) : 
+        indFig = plt.figure() 
+        indAx = indFig.add_axes([0.1, 0.1, 0.7, 0.8])
+        indFig.text(0.5,0.05, "Time (ns)", ha='center', va='center') 
+        indFig.text(0.05,0.5, r"Helical fraction",ha='center',va='center',rotation='vertical') 
+        ax = axarr[row,col]
+        datafile = '%s_%s/%s'%(state,solvent,inFile) 
+        try : 
+            data = np.genfromtxt(datafile) 
+        except IOError : 
+            print "No file found for %s %s"%(state,solvent) 
+            index+=1
+            continue 
+
+        frameNumber = len(data)
+        assert frameNumber > 0 
+
+        binNumber = frameNumber/binSize
+
+        print "Done reading data...binning" 
+
+        y1 = data[:,1] # Alpha 
+        y2 = data[:,2] # 3_10 Helix
+        y3 = data[:,3] ##unfolded
+        
+        y1 /= 18 
+        y2 /= 18 
+        y3 /= 18 
+        
+        y1binned =[] 
+        y2binned =[] 
+        y3binned =[] 
+
+
+        print frameNumber, " frames"
+        print binSize, " frames per bin"
+        print binNumber, "bins to be graphed"
+
+        for j in range(binNumber):
+            sum1 = 0
+            sum2 = 0
+            sum3 = 0
+        
+            for i in range(binSize):
+                sum1 += y1[i + j*binSize]
+                sum2 += y2[i + j*binSize] 
+                sum3 += y3[i + j*binSize] 
+        
+            y1binned.append(sum1/binSize)
+            y2binned.append(sum2/binSize)
+            y3binned.append(sum3/binSize)
+
+        y1binned = np.array(y1binned)
+        y2binned = np.array(y2binned)
+        y3binned = np.array(y3binned)
+
+        print "Data binned.... now plotting"
+        #rc_file('%s/rc_files/%s'%(projectDir,rcFile) ) 
+        
+        x = data[:,0] 
+        x = np.linspace(0,np.max(data[:,0]),binNumber) 
+        x /= 1000 
+        #x = np.linspace(0,binSize*binNumber/ 10,binNumber) 
+        
+        ax.fill_between(x,0                ,y3binned         ,facecolor='r',linewidth=0.0)
+        ax.fill_between(x,y3binned         ,y3binned+y2binned,facecolor='g',linewidth=0.0) 
+        ax.fill_between(x,y3binned+y2binned,1                ,facecolor='b',linewidth=0.0) 
+
+        indAx.fill_between(x,0                ,y3binned         ,facecolor='r',linewidth=0.0)
+        indAx.fill_between(x,y3binned         ,y3binned+y2binned,facecolor='g',linewidth=0.0) 
+        indAx.fill_between(x,y3binned+y2binned,1                ,facecolor='b',linewidth=0.0) 
+        
+        print "Data plotted.... now labeling"
+        
+        ax.set_xlim(0,np.max(x)) 
+        ax.set_ylim(0,1)
+        ax.set_title("%s %s"%(state,solvent)) 
+
+        indAx.set_xlim(0,np.max(x)) 
+        indAx.set_ylim(0,1)
+        indAx.set_title("%s %s"%(state,solvent)) 
+    
+        indFig.legend( [blue,green,red],[r"$\alpha$-helix",r"$3_{10}$-helix",r"unfolded"], 
+            loc = 'center', bbox_to_anchor=(0.90, 0.5),
+            fontsize='medium') 
+        indFig.savefig("%s/binned_%s_%s.pdf"%(saveDir,state,solvent), format='pdf')
+        plt.close(indFig) 
+
+        index+=1
+
+#fig.legend(handles=[blue, green, red],loc=4)
+fig.legend( [blue,green,red],[r"$\alpha$-helix",r"$3_{10}$-helix",r"unfolded"], 
+    loc = 'center', bbox_to_anchor=(0.90, 0.5),
+    fontsize='medium') 
 
 print "Plot labeled.... now saving"
 
-plt.savefig(outname, format='pdf')
+fig.savefig(outname, format='pdf')
 plt.close()
 print "plot saved to ", outname
 
