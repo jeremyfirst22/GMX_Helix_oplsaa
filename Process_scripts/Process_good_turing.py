515896cd2d8c480555bd80a7555e0a3e1cb6ae2b
jeremyfirst22 <jeremy_first@utexas.edu>
2019-02-11 09:31:44 -0600

    This turned out to be too difficult to keep consistent without having variables change for each and every trajectory. Abandoned this idea, but work is saved for future reference

--- Process_scripts/Process_good_turing.py
+++ Process_scripts/Process_good_turing.py
@@ -4,46 +4,60 @@ import matplotlib.pyplot as plt
 import os
 from os import sys
 from matplotlib.colors import LogNorm
+from matplotlib import rc_file
 import matplotlib.lines as mlines 
 
 cutoff = 10 ##Electrostatic cutoff distance (Angstroms) 
 
 figCols=1
-figRows=3
+figRows=1
+
+rc_file('rc_files/paper.rc') 
 
 if not os.path.isdir('figures') : 
     os.mkdir('figures') 
 
-fig, axarr = plt.subplots(figRows,figCols,sharex='col',sharey='row') 
-fig.subplots_adjust(left=0.1,right=0.8,top=0.9,bottom=0.1) 
-fig.text(0.5,0.05, "RMSD ($\AA$)", ha='center', va='center') 
-fig.text(0.05,0.5, r"Prob of unobserved", ha='center', va='center',rotation='vertical') 
+colorDict = {'water':'k','tert':'b','not_bound_sam':'g'}
+
+left, right = 0.13,0.95
+bottom, top = 0.13,0.95
+
+fig, ax = plt.subplots(1,1)
+fig.subplots_adjust(left=left, bottom=bottom,right=right,top=top)
+fig.text((right-left)/2+left,0.00,           r"RMSD ($\AA$)", ha='center', va='bottom')
+fig.text(0.00,(top-bottom)/2+bottom,         r"Probability of unobserved structure",ha='left',va='center',rotation='vertical')
 
 index=0
 xmax=50##Starting xlim
-for solvent in ['water','tert','sam'] : 
-    ax = axarr[index%figRows]
-    for state in ['folded','unfolded'] : 
-        datafile = '%s/%s/good-turing/good_turing.rmsd.P_unobserved_vs_RMSD.dat'%(solvent,state) 
+for solvent in ['water','tert','not_bound_sam'] : 
+#    ax = axarr[index%figRows]
+#    for state in ['folded','unfolded'] : 
+        datafile = '%s/good_turing/good_turing.rmsd.P_unobserved_vs_RMSD.dat'%(solvent) 
 
         try : 
             
             data = np.genfromtxt(datafile,skip_header=1) 
         except IOError : 
-            print "No file found for %s %s"%(state,solvent)  
+            print "No file found for %s"%(solvent) 
             continue
-        if state == 'folded' : 
-            c='b' 
-        else :
-            c='g'
+        #if state == 'folded' : 
+        #c='b' 
+        #else :
+        #    c='g'
         
         if data[:,0].max() > xmax : 
             xmax = data[:,0].max() 
 
-        ax.errorbar(data[:,0],data[:,1],yerr=data[:,2],color=c,label=state) 
-        ax.set_title('%s'%(solvent) ) 
+        try : 
+            ax.errorbar(data[:,0],data[:,1],yerr=data[:,2],color=colorDict[solvent]) #,label=state) 
+        except IndexError : 
+            ax.scatter(data[:,0],data[:,1],s=0.5,color=colorDict[solvent]) #,label=state) 
+            ax.plot(data[:,0],data[:,1],color=colorDict[solvent]) #,label=state)    
+    #ax.set_title('%s'%(solvent.replace('_',' ')) ) 
+ax.set_ylim(0,1)
+ax.set_xlim(0,4.5)
 
-    index +=1
+#index +=1
 
 #blu_dot = mlines.Line2D([],[], linestyle='None',color='b', marker='o',label="folded") 
 #gre_dot = mlines.Line2D([],[], linestyle='None',color='g', marker='o',label="unfolded") 
