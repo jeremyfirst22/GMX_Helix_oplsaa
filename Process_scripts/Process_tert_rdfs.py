36bfe94e02d699525251b55d23a81634cd3f25dc
jeremyfirst22 <jeremy_first@utexas.edu>
2019-02-21 13:27:21 -0600

    Combined three plots into one figure

--- Process_scripts/Process_tert_rdfs.py
+++ Process_scripts/Process_tert_rdfs.py
@@ -12,19 +12,25 @@ ymax = {
 rc_file('rc_files/paper.rc') 
 
 
-figRows, figCols = 3,1
-left, right = 0.15,0.95
-bottom, top= 0.05,0.98
-fig, axarr = plt.subplots(figRows, figCols, sharex='all',sharey='row', figsize=(3.0,6.0))
+figRows, figCols = 1,1
+left, right = 0.10,0.95
+bottom, top= 0.15,0.95
+fig, ax    = plt.subplots(figRows, figCols, sharex='all',sharey='row', figsize=(3.25,2.00))
 fig.subplots_adjust(left=left, bottom=bottom,right=right,top=top)
 fig.subplots_adjust(wspace=0,hspace=0.1)
 
-fig.text((right-left)/2+left,0,           r"g(r)", ha='center', va='bottom')
-fig.text(0,(top-bottom)/2+bottom,         r"r ($\AA$)",ha='left',va='center',rotation='vertical')
+fig.text((right-left)/2+left,0.01,           r"g(r)", ha='center', va='bottom')
+fig.text(0.01,(top-bottom)/2+bottom,         r"r ($\AA$)",ha='left',va='center',rotation='vertical')
 
-for index,item in enumerate(['tba_tba', 'tba_wat', 'wat_wat']) : 
-    ax = axarr[index]
-    ax.text(0.95,0.95,item.replace('_','-').replace('tba','TBA').replace('wat','H$_2$O'),transform=ax.transAxes,ha='right',va='top')
+colors = {
+        'tba_tba':'b',
+        'tba_wat':'g',
+        'wat_wat':'r' 
+        }
+
+for index,item in enumerate(['wat_wat', 'tba_wat', 'tba_tba']) : 
+#    ax = axarr[index]
+    #ax.text(0.95,0.95,item.replace('_','-').replace('tba','TBA').replace('wat','H$_2$O'),transform=ax.transAxes,ha='right',va='top')
 
     fileName = "rdfs_for_tert_validation/%s.xvg"%item
     headlines = 0 
@@ -38,11 +44,15 @@ for index,item in enumerate(['tba_tba', 'tba_wat', 'wat_wat']) :
     data[:,0] *= 10
     y = np.full_like(data[:,0],1)
 
-    ax.plot(data[:,0],data[:,1],color='m',linewidth=2)#,s=0.1 
+    ax.plot(data[:,0],data[:,1],linewidth=2,
+            color=colors[item],
+            label=item.replace('_','-').replace('tba','TBA').replace('wat','H$_2$O'))#,s=0.1 
     ax.plot(data[:,0],y,color='k') 
     
-    ax.set_ylim(0,ymax[item]) 
+    ax.set_ylim(0,6) 
     ax.set_xlim(0,20)
+
+ax.legend(loc=1) 
     
 plt.savefig("figures/rdfs.png",format='png') 
 
